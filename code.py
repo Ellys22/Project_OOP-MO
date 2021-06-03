ddb56fee277602ee2df350b616204c1c88f5a9da
Ellys22 <sba21167@student.cct.ie>
2021-06-03 15:10:44 +0600

    Created condition for search

--- code.py
+++ code.py
@@ -1,16 +1,37 @@
-from daftlistings import Daft, Location, SearchType
+from daftlistings import Daft, Location, SearchType, PropertyType, Facility, Ber
 
-daft = Daft()
-daft.set_location(Location.DUBLIN_CITY_CENTRE_DUBLIN)
-daft.set_search_type(SearchType.STUDENT_ACCOMMODATION)
-daft.set_search_type(SearchType.SHARING)
 
-listings = daft.search()
 
-for listing in listings:
-    print(listing.title)
-    print(listing.price)
-    print(listing.daft_link)
+# location Dublin 1 and/or Dublin2
+# - price : if is need it a range can be between 500€ - 1000€
+# - apartment 1 room
+# - studio
+# - single or double bed
+# - shared room in apartment
+
+locationList = [Location.DUBLIN_1_DUBLIN, Location.DUBLIN_2_DUBLIN]
+typeList = [PropertyType.HOUSE, PropertyType.STUDIO_APARTMENT]
+facilityList = [Facility.INTERNET, Facility.CENTRAL_HEATING]
+searchList = [SearchType.STUDENT_ACCOMMODATION, SearchType.SHARING]
+
+def getData():
+    daft = Daft()
+    daft.set_min_price(500)
+    daft.set_max_price(1000)
+    daft.set_max_beds(2)
+    daft.set_facility(Facility.INTERNET)
+    
+    listings = daft.search()
+    print(len(listings))
+
+
+    for listing in listings:
+        print(listing.title)
+        print(listing.price)
+        print(listing.daft_link)
+        print(listing.bedrooms)
+        print(listing.brochure)
+        print()
 
 
 # git push https://Ellys22:ETscorpion22@20@github.com/B_user_name/project.git
